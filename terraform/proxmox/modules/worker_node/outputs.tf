57d5a11d35bfd7c8aec4ff58427daaa43ca97f5b
shri021122 <shrikantfiverr@gmail.com>
2026-05-28 18:52:29 +0530

    Reset master: branches contain the actual configurations

--- terraform/proxmox/modules/worker_node/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "vm_id"      { value = proxmox_virtual_environment_vm.worker.vm_id }
-output "hostname"   { value = proxmox_virtual_environment_vm.worker.name }
-output "ip_address" { value = var.ip_address }
